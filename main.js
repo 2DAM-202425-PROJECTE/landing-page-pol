6e6690ad81ac2ca3a46a1aca18bf31fee3e4fee2
ptordera <poltordera@iesebre.com>
2024-10-05 20:20:10 +0200

    Fletxa go on top + fix #10

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const loader = document.getElementById('loading');
+    if (loader) {
+        loader.style.display = 'none';
+    }
+});
