8a2fc7a08fbc4489627a7697578e4ccf278bd3ef
ptordera <poltordera@iesebre.com>
2024-10-07 20:10:50 +0200

    Fix #14

--- main.js
+++ main.js
@@ -14,20 +14,21 @@ const btn = document.getElementById('button');
 document.getElementById('contact').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    document.getElementById("responseMessage").classList.add("hidden");
+    document.getElementById("errorMessage").classList.add("hidden");
+
     const btn = document.getElementById('button');
     btn.value = 'Sending...';
 
     const serviceID = 'service_ap02tii';
     const templateID = 'template_qa363di';
 
-    emailjs.sendForm(serviceID, templateID, this).then(
-        () => {
-            btn.value = 'Send Email';
-            alert('Sent!');
-        },
-        (err) => {
-            btn.value = 'Send Email';
-            alert(JSON.stringify(err));
-        }
-    );
+    emailjs.sendForm(serviceID, templateID, this).then(function(response) {
+        console.log("Success!", response.status, response.text);
+        document.getElementById("responseMessage").classList.remove("hidden");
+        document.getElementById("contactForm").reset();
+    }, function(error) {
+        console.error("Error:", error);
+        document.getElementById("errorMessage").classList.remove("hidden");
+    });
 });
