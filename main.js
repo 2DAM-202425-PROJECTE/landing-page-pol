03c74270b82dc530d71153a1fff54d0b4cbfbc9f
ptordera <poltordera@iesebre.com>
2024-10-07 19:38:47 +0200

    Formulari Contact Funciona Fix #13

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
         loader.style.display = 'none';
     }
 });
+
+const btn = document.getElementById('button');
+
+document.getElementById('contact').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const btn = document.getElementById('button');
+    btn.value = 'Sending...';
+
+    const serviceID = 'service_ap02tii';
+    const templateID = 'template_qa363di';
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+        () => {
+            btn.value = 'Send Email';
+            alert('Sent!');
+        },
+        (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        }
+    );
+});
